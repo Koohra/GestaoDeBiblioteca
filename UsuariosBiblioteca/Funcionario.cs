c662cefd4976742f03ffaedf834ddac1b9b920b9
czambanini <ca.zambanini@gmail.com>
2023-12-19 17:29:33 -0300

    alterações em usuario e funcionario

--- UsuariosBiblioteca/Funcionario.cs
+++ UsuariosBiblioteca/Funcionario.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace UsuariosBiblioteca
 {
-    internal class Funcionario
+    internal class Funcionario : Usuario
     {
+        public string Login { get; set; }
+        private string Senha {  get; set; }
+
+        public Funcionario(string nome, string login, string senha) : base(nome)
+        {
+            Login = login;
+            Senha = senha;
+        }
+
+        public void VerificarStatusLivro()
+        {
+            Console.WriteLine("Digite nome do livro desejado:");
+            string busca = Console.ReadLine();
+            //essas coisas ainda não existem então editar para nomes certos quando existirem. Isso fica aqui ou em livros e só é puxado?
+            //bool encontrado = false;
+            //foreach (Livro livro in acervo)
+            //{
+            //    if (livro.nome.Contains(busca) == true)
+            //    {
+            //        Console.WriteLine($"{livro.nome} - {livro.status}");
+            //        encontrado = true;
+            //    }
+            //}
+            //if (encontrado == false)
+            //{
+            //    Console.WriteLine("Livro não encontrado.");
+            //}
+            //tratar para ser aceito tanto letras maiusculas quanto minusculas.
+
+        }
+
+        public void CadastrarLivro()
+        {
+            //chama função de livro
+        }
+
+        public void AtualizarNumeroLivros()
+        {
+
+        }
+
+        //public void ValidarSenha()
+        //{
+        //    isso fica aqui ou em interface?
+        //}
     }
 }
--- UsuariosBiblioteca/Usuario.cs
+++ UsuariosBiblioteca/Usuario.cs
@@ -1,7 +1,18 @@
 ﻿namespace UsuariosBiblioteca
 {
-    public class Usuario
+    public abstract class Usuario
     {
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
 
+        public Usuario(string nome)
+        {
+            Nome = nome;
+        }
+
+        public virtual void PesquisarLivro()
+        {
+            //implementar
+        }
     }
 }
