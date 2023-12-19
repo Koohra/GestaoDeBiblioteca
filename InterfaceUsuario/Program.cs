3b3a1c330bb4ea4e344876342406b043c79129a6
Miguel Pereira de Sousa <miguel.pereira.12@live.com>
2023-12-19 11:29:25 -0300

    Add project files.

---
+++ InterfaceUsuario/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InterfaceUsuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
