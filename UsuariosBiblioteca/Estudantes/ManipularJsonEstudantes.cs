95a18075003cecee83055012a8ba86a3b7a11f11
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2023-12-29 09:20:07 -0300

    .

--- UsuariosBiblioteca/Estudantes/ManipularJsonEstudantes.cs
+++ UsuariosBiblioteca/Estudantes/ManipularJsonEstudantes.cs
@@ -1,22 +1,17 @@
 ﻿using Newtonsoft.Json;
 using UsuariosBiblioteca.Estudantes;
 
-
-
 namespace SistemaBiblioteca.Dados
 {
     internal class ManipularJsonEstudantes
     {
         public string? Caminho { get; set; }
 
-
-
         public string EstudanteService(string arquivoJson = "ListaDeEstudantes.json")
         {
             Caminho = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Dados", arquivoJson);
             Caminho = Caminho.Replace("bin\\Debug\\net8.0\\", "");
             return Caminho;
-
         }
 
         public List<Estudante>? LerJsonEstudantes()
@@ -29,7 +24,6 @@ namespace SistemaBiblioteca.Dados
                     List<Estudante>? estudantes = JsonConvert.DeserializeObject<List<Estudante>>(conteudoJson);
                     return estudantes;
                 }
-
                 else
                 {
                     Console.WriteLine("O arquivo json não foi encontrado");
@@ -41,8 +35,6 @@ namespace SistemaBiblioteca.Dados
                 Console.WriteLine($"Erro ao ler o json de Estudantes: {e}");
                 return new List<Estudante>();
             }
-
-
         }
     }
 }
